21c3e0d68a57cd6b7e134d1f026d996a867484ca
thiago <thiagoalexandredornelas@gmail.com>
2024-10-11 10:50:05 -0300

    Update validarProduto.js

--- validarProduto.js
+++ validarProduto.js
@@ -1,17 +1,1 @@
-const Ajv = require('ajv')
-const ajv = new Ajv()
 
-const produtoSchema = require("../schema/Produto.Schema")
-
-function validarProduto(req, res, next){
-    const post = req.body
-    const validate = ajv.compile(produtoSchema)
-    const valid = validate(post)
-    if (valid){
-        next()
-    }else{
-        res.status(400).json({msg: "Dados inválidos", erros: validate.errors})
-    }
-}
-
-module.exports = validarProduto
